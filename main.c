2ffd011f9e57e4352df9d85aa153cc42134dd58f
juanvalentindev <juanvalentinquiroga@gmail.com>
2026-09-01 17:49:50 -0300

    Implementación parcial  de LSO, creación de bisección , e implementación de alta en LSO

--- main.c
+++ main.c
@@ -9,14 +9,14 @@
 
 //Definicion de struc's Patron
 typedef struct {
-   int dni; //DNI es el dato x que identifica al struct
-   char[50] nombre;
-   char[50] apellido;
-   char[80] domicilio;
+   long dni; //DNI es el dato x que identifica al struct
+   char nombre [50];
+   char apellido [50] ;
+   char domicilio[80];
    int codigoPostal;
    int numeroMesa;
    int circuito;
-} elector;
+}elector;
 
 //Lista Secuencialmente Ordenada (LSO)
 typedef struct{
@@ -25,17 +25,72 @@ typedef struct{
 } lso;
 
 
+//Localizar LSO
+/*Directrices
+limite inferior inclusivo, l ́ımi-
+te superior inclusivo, testigo a izquierda y segmento mas grande a la izquierda.*/
+
+void LocalizarLSO(lso *lista, int *pos, long dni, int *exito){
+  int li = 0;
+  int ls = (lista->cantidad-1);
+  int t;
+
+  while (li<=ls){
+    t = (li + ls)/2; //Testigo a la izquierda
+
+    if (lista->electores[t].dni < dni){
+        li = t + 1;
+    }else{
+        ls = t - 1;
+    }
+  }
+
+  *pos = t;
+  if(lista->electores[li].dni == dni){
+    *exito = 1;
+  }else{
+    *exito = 0;
+  }
+}
+
+//Alta LSO
+void AltaLSO(lso *lista, long dni, int *exito){
+    int pos;
+    int exitoPrima;
+    LocalizarLSO(lista,&pos,dni,&exitoPrima);
+
+
+    if(exitoPrima == 1){
+        exitoPrima == 0;
+
+        if (lista->electores[pos].dni == dni){
+            *exito = 0;
+        }
+
+    }else{
+        if(lista->cantidad != ELECTORES_ESPERADOS){
+            //Agregar logica de corrimiento + contador de costos
+            *exito = 1;
+        }else{
+            *exito = 0;
+        }
+
+
+    }
+}
+
+//Baja LSO
+void BajaLSO(){
+
+
+}
+
+
 int main(){
-
     //Definición de Estructuras
-
     //Lista secuencialmente ordenadas
-
     lso listaSecuencialOrdenada;
 
-
-
-    printf("Hello world!\n");
     return 0;
 
 }
