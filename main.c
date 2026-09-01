a16f4a19ac513d8b37a9794e6897828435bf0e8d
juanvalentindev <juanvalentinquiroga@gmail.com>
2026-09-01 15:09:22 -0300

    Preinicialización de variables, constantes, y definición de la lista secuencial ordenada

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
+
+// Librerias
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
-int main()
-{
+//Constantes
+#define ELECTORES_ESPERADOS 2000
+
+//Definicion de struc's Patron
+typedef struct {
+   int dni; //DNI es el dato x que identifica al struct
+   char[50] nombre;
+   char[50] apellido;
+   char[80] domicilio;
+   int codigoPostal;
+   int numeroMesa;
+   int circuito;
+} elector;
+
+//Lista Secuencialmente Ordenada (LSO)
+typedef struct{
+  elector electores[ELECTORES_ESPERADOS];
+  int cantidad;
+} lso;
+
+
+int main(){
+
+    //Definición de Estructuras
+
+    //Lista secuencialmente ordenadas
+
+    lso listaSecuencialOrdenada;
+
+
+
     printf("Hello world!\n");
     return 0;
+
 }
