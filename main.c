2b6f77c616ac16c03195300bfc22c35023e30d04
Ezequiel <ezequielwanzo2508@gmail.com>
2026-09-12 17:47:08 -0300

    Actualizacion completa de LSO y el struct del padron.

--- main.c
+++ main.c
@@ -19,21 +19,20 @@ static float costoTotalLSO = 0;
 
 //Definicion de struc's Patron
 typedef struct {
-   long dni; //DNI es el dato x que identifica al struct
-   char nombre [50];
-   char apellido [50] ;
-   char domicilio[80];
-   int codigoPostal;
-   int numeroMesa;
-   int circuito;
-}elector;
-
+   long dni;                 // El D.N.I. es un entero (usamos long por el tamaño del número)
+   char nombreApellido[50];  // Secuencia de hasta 50 caracteres
+   char domicilio[80];       // Secuencia de hasta 80 caracteres
+   int codigoPostal;         // Es un entero
+   int numeroMesa;           // Es un entero
+   int circuito;             // Es un entero
+} elector;
 
 /*
 |
 | OTROS
 |
 |   */
+
 
 int CompararNombresNoCaseSensitive(const char *str1, const char *str2) {
     while (*str1 && *str2) {
@@ -52,18 +51,20 @@ int CompararNombresNoCaseSensitive(const char *str1, const char *str2) {
    }
 }
 
-int CompararNuplasCompletas(elector nuplaArbol, elector nuplaBajar) {
-    // Verificamos la parte X (Identificador)
-    if (nuplaArbol.dni != nuplaBajar.dni) {return 0;}
-    // Verificamos la parte Y (Resto de los atributos
-    if (CompararNombresNoCaseSensitive(nuplaArbol.nombre, nuplaBajar.nombre) == 0) {return 0;}
-    if (CompararNombresNoCaseSensitive(nuplaArbol.apellido, nuplaBajar.apellido) == 0) {return 0;}
-    if (CompararNombresNoCaseSensitive(nuplaArbol.domicilio, nuplaBajar.domicilio) == 0) {return 0;}
-    // Para tipos numéricos nativos, usamos el operador tradicional
-    if (nuplaArbol.codigoPostal != nuplaBajar.codigoPostal) {return 0;}
-    if (nuplaArbol.numeroMesa != nuplaBajar.numeroMesa) {return 0;}
-    if (nuplaArbol.circuito != nuplaBajar.circuito) {return 0;}
-    return 1; // Son iguales
+int CompararNuplas(elector e1, elector e2) {
+    // Comparamos los campos numéricos con ==
+    if (e1.dni == e2.dni &&
+        e1.codigoPostal == e2.codigoPostal &&
+        e1.numeroMesa == e2.numeroMesa &&
+        e1.circuito == e2.circuito &&
+        // Comparamos las cadenas ignorando mayúsculas/minúsculas
+        CompararNombresNoCaseSensitive(e1.nombreApellido, e2.nombreApellido) == 1 &&
+        CompararNombresNoCaseSensitive(e1.domicilio, e2.domicilio) == 1) {
+
+        return 1; // Las nuplas son idénticas
+    }
+
+    return 0; // Hay diferencias, no son la misma persona
 }
 
 
@@ -89,15 +90,16 @@ typedef struct{
 .te superior inclusivo, testigo a izquierda y segmento mas grande a la izquierda.
 */
 
-void LocalizarLSO(lso *lista, int *pos, long dni, int *exito, float *costoTotalLSO){
+void LocalizarLSO(lso *lista, int *pos, long dni, int *exito, float *costoConsultasLSO){
     int li = 0;
     int ls = (lista->cantidad - 1);
     int t = (li + ls) / 2;
 
     while (li <= ls) {
-        (*costoTotalLSO)++; //Costo por consultar la celda t
+        (*costoConsultasLSO)++; // Sumamos 1 por consultar la celda t
+
         if (lista->electores[t].dni == dni) {
-            break; //Si esta, corta
+            break;
         }
 
         if (lista->electores[t].dni < dni){
@@ -107,6 +109,7 @@ void LocalizarLSO(lso *lista, int *pos, long dni, int *exito, float *costoTotalL
         }
         t = (li + ls) / 2;
     }
+
     if (li <= ls) {
         *exito = 1;
         *pos = t;
@@ -117,40 +120,48 @@ void LocalizarLSO(lso *lista, int *pos, long dni, int *exito, float *costoTotalL
 }
 
 //Baja LSO
-void BajaLSO(lso *lista, long dniBuscar, int *exito, float *costoTotalLSO){
+void BajaLSO(lso *lista, elector eBorrar, int *exito, float *costoCorrimientosLSO){
     int pos;
 
-    LocalizarLSO(lista, &pos, dniBuscar, exito,costoTotalLSO);
+    // 1. Buscamos por la clave X (DNI)
+    LocalizarLSO(lista, &pos, eBorrar.dni, exito, costoCorrimientosLSO);
 
+    // 2. Si se encontró el DNI, confirmamos por código comparando toda la nupla
     if (*exito == 1) {
-        for (int j = pos; j < lista->cantidad - 1; j++) {
-            lista->electores[j] = lista->electores[j + 1];
-            (*costoTotalLSO)++;
+        elector almacenado = lista->electores[pos];
+
+        // AQUÍ INVOCAMOS A TU FUNCIÓN RECICLABLE
+        if (CompararNuplas(almacenado, eBorrar) == 1) {
+
+            // Si todo coincide, hacemos la baja con sus corrimientos
+            for (int j = pos; j < lista->cantidad - 1; j++) {
+                lista->electores[j] = lista->electores[j + 1];
+                (*costoCorrimientosLSO)++;
+            }
+            lista->cantidad--;
+            *exito = 1;
+
+        } else {
+            *exito = 0; // Fracasa porque el resto de la nupla no coincide
         }
-        lista->cantidad--;
     }
 }
 //Alta LSO
-void AltaLSO(lso *lista, elector nuevoElector, int *exito, float *costoTotalLSO){
+void AltaLSO(lso *lista, elector nuevoElector, int *exito, float *costoCorrimientosLSO){
     int pos;
     int exitoPrima;
 
-    LocalizarLSO(lista, &pos, nuevoElector.dni, &exitoPrima,costoTotalLSO);
+    LocalizarLSO(lista, &pos, nuevoElector.dni, &exitoPrima, costoCorrimientosLSO);
 
     if (exitoPrima == 1) {
-        *exito = 2; //Fracasa por elemento repetido
-
+        *exito = 2; // Repetido
     } else if (lista->cantidad == ELECTORES_ESPERADOS) {
-        *exito = 3; //Fracasa por falta de espacio
-
+        *exito = 3; // Sin espacio
     } else {
-
         for (int j = lista->cantidad; j > pos; j--) {
             lista->electores[j] = lista->electores[j - 1];
-            (*costoTotalLSO)++; // Sumamos 1 al costo por cada movimiento
+            (*costoCorrimientosLSO)++;
         }
-
-        // Insertamos la nupla completa en la posición liberada
         lista->electores[pos] = nuevoElector;
         lista->cantidad++;
         *exito = 1;
